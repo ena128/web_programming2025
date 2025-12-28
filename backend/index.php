1726aff5c0b03d0a25a9662a558899eabd60665a
Ena <ena.slipicevic@stu.ibu.edu.ba>
2025-12-28 22:50:02 +0100

    fixed paths

--- backend/index.php
+++ backend/index.php
@@ -1,4 +1,13 @@
 <?php
+
+header('Access-Control-Allow-Origin: https://sea-lion-app-z5bja.ondigitalocean.app/'); 
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit;
+}
 // Autoload Composer dependencies
 require __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/config.php';
